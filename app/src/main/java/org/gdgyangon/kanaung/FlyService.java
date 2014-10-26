752db5694044bf4f622fa4a85f9f6f1ba93db70c
Ye Myat Thu <thu.yemyat@gmail.com>
2014-10-27 02:40:45 +0630

    remove unnecessary comments

--- app/src/main/java/org/gdgyangon/kanaung/FlyService.java
+++ app/src/main/java/org/gdgyangon/kanaung/FlyService.java
@@ -149,17 +149,9 @@ public class FlyService extends Service {
                         .setSmallIcon(R.drawable.unicode)
                         .setContentTitle(getString(R.string.app_name))
                         .setContentText("Kanaung is running.");
-// Creates an explicit intent for an Activity in your app
         Intent resultIntent = new Intent(getApplicationContext(), MainActivity.class);
-
-// The stack builder object will contain an artificial back stack for the
-// started Activity.
-// This ensures that navigating backward from the Activity leads out of
-// your application to the Home screen.
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-// Adds the back stack for the Intent (but not the Intent itself)
         stackBuilder.addParentStack(MainActivity.class);
-// Adds the Intent that starts the Activity to the top of the stack
         stackBuilder.addNextIntent(resultIntent);
         PendingIntent resultPendingIntent =
                 stackBuilder.getPendingIntent(
@@ -169,7 +161,6 @@ public class FlyService extends Service {
         mBuilder.setContentIntent(resultPendingIntent);
         NotificationManager mNotificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-// mId allows you to update the notification later on.
         mNotificationManager.notify(2, mBuilder.build());
     }
 }
